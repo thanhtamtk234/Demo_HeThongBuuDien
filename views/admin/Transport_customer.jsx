a93f1609cc791f4275e4481bef25209c67ca9912
thanh_tam <thanhtamtk234@gmail.com>
2019-06-14 17:02:40 +0700

    demo

--- views/admin/Transport_customer.jsx
+++ views/admin/Transport_customer.jsx
@@ -1,43 +1,42 @@
 var React = require('react');
 
 import Css from '../layouts/Css';
-import AdminLayout from './AdminLayout';
 import Footer from '../layouts/Footer';
+import EmployeesLayout from './EmployeesLayout';
 
 
-class Users extends React.Component {
+class Transport_customer extends React.Component {
     render() {
         return (
-
-            <AdminLayout>
-                <Css />
+            <EmployeesLayout>
+                 <Css/>
                 <div className="">
                     <ol className="breadcrumb">
                         <li className="breadcrumb-item">
                             <a href="/admin">Trang chủ</a>
                         </li>
                         <li className="breadcrumb-item">
-                            <a href="/admin/user">Quản lí nhân viên</a>
+                            <a href="/admin/user">Quản lí đơn vận</a>
                         </li>
 
                     </ol>
                 </div>
                 <div className="content">
 
-                    <div>
-                        <a href="/them" class="btn btn-info btn-lg">
-                            <span class="glyphicon glyphicon-plus"></span> Thêm
-                        </a>
-                    </div>
+                <form action="">
+  <input className="search" type="text" placeholder="Search.." name="search" />
+  <button type="submit"><i className="fa fa-search" /></button>
+        </form>
                     <table className="table ">
                         <thead>
                             <tr>
-                                <th>Mã nhân viên</th>
-                                <th>Mã bưu cục</th>
-                                <th>Họ và tên</th>
-                                <th className="diachi">Địa chỉ</th>
-                                <th>Email</th>
+                                <th>Mã đơn hàng</th>
+                                <th>Họ tên người gửi</th>
                                 <th>Số điện thoại</th>
+                                <th >Địa chỉ người gửi</th>
+                                <th>Họ tên người nhận</th>
+                                <th>Số điện thoại</th>
+                                <th>Địa chỉ người nhận</th>
                                 <th className="action"></th>
 
                             </tr>
@@ -46,34 +45,35 @@ class Users extends React.Component {
                             {this.props.result.map((item, key) =>
                                 <tr key={key}>
                                     <td
-                                        scope="row">{item.id}
+                                        scope="row">{item.id_transport}
 
                                     </td>
                                     <td>
-                                        {item.MaBuuCuc}
+                                        {item.Name_Sender}
                                     </td>
                                     <td>
-                                        {item.username}
+                                        {item.Phone_Sender}
                                     </td>
                                     <td>
-                                        {item.DiaChi}
+                                         {item.Adress_Sender}
+                                       
                                     </td>
                                     <td>
-                                        {item.Email}
+                                        {item.Name_Receiver}
                                     </td>
                                     <td>
-                                        {item.Sdt}
+                                        {item.Adress_Receiver}
                                     </td>
                                     <td>
-                                        <a href="#" className="btn btn-primary a-btn-slide-text-sua">
+                                        {item.Phone_Receiver}
+                                    </td>
+                                    <td>
+                                        <a href="/transport_employees" className="btn btn-primary a-btn-slide-text-sua">
                                             <span className="glyphicon glyphicon-edit" aria-hidden="true" />
                                             <span><strong>Sửa</strong></span>
                                         </a>
-                                        {/* <a href="#" className="btn btn-primary a-btn-slide-text-xem">
-                                        <span className="glyphicon glyphicon-eye-open" aria-hidden="true" />
-                                        <span><strong>Xem</strong></span>            
-                                    </a> */}
-                                        <a href={"/admin/user/delete/" + item.id} className="btn btn-primary a-btn-slide-text-xoa" >
+
+                                        <a href={"/quanlidonvan/delete/" + item.id_transport} className="btn btn-primary a-btn-slide-text-xoa" >
                                             <span className="glyphicon glyphicon-remove" aria-hidden="true" />
                                             <span><strong>Xóa</strong></span>
                                         </a>
@@ -84,7 +84,9 @@ class Users extends React.Component {
                     </table>
                 </div>
                 <Footer />
-            </AdminLayout>
+            </EmployeesLayout>
+             
+           
 
 
 
@@ -94,4 +96,4 @@ class Users extends React.Component {
     }
 }
 
-module.exports = Users;
+module.exports = Transport_customer;
--- views/layouts/Css.jsx
+++ views/layouts/Css.jsx
@@ -14,7 +14,7 @@ class Css extends Component {
                 />
                 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossOrigin="anonymous"></link>
                 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossOrigin="anonymous"></link>
-                <link rel="stylesheet" href="style.css"></link>
+                
                 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"></link>
                 <link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet"></link>
             </head>
@@ -29,11 +29,11 @@ class Css extends Component {
 
 
 
-<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossOrigin="anonymous"></script>
 
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js" integrity="sha384-cs/chFZiN24E4KMATLdqdvsezGxaGsi4hLGOzlXwp5UZB1LY//20VyM2taTB4QvJ" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js" integrity="sha384-cs/chFZiN24E4KMATLdqdvsezGxaGsi4hLGOzlXwp5UZB1LY//20VyM2taTB4QvJ" crossOrigin="anonymous"></script>
 
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js" integrity="sha384-uefMccjFJAIv6A+rW+L4AHf99KvxDjWSu1z9VI8SKNVmz4sk7buKt/6v9KI65qnm" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js" integrity="sha384-uefMccjFJAIv6A+rW+L4AHf99KvxDjWSu1z9VI8SKNVmz4sk7buKt/6v9KI65qnm" crossOrigin="anonymous"></script>
 
 </body>
         </html>
